adf1e31872c905ecae67b013a61cd6081c39e1b3
grinnch <srirachaexpress@gmail.com>
2025-11-20 23:53:02 -0500

    Bump version to 1.6.1

--- __init__.py
+++ __init__.py
@@ -30,7 +30,7 @@ bl_info = {
     "blender": (3, 1, 0),
     "category": "Object",
     "author": "grinnch (@meangrinch)",
-    "version": (1, 6, 0),
+    "version": (1, 6, 1),
     "location": "3D View > UI > Gemini Blender Assistant",
     "description": "Generate Blender Python code using Google's Gemini.",
     "wiki_url": "",
