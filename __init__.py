14b02565833eb54117d39332a5df7a9ff86db075
grinnch <125788565+meangrinch@users.noreply.github.com>
2026-07-21 17:24:08 -0400

    chore: bump version to 1.7.2

--- __init__.py
+++ __init__.py
@@ -31,7 +31,7 @@ bl_info = {
     "blender": (3, 1, 0),
     "category": "Object",
     "author": "grinnch (@meangrinch)",
-    "version": (1, 7, 1),
+    "version": (1, 7, 2),
     "location": "3D View > UI > Gemini Blender Assistant",
     "description": "Generate Blender Python code using Google's Gemini.",
     "wiki_url": "",
